e237a645dc09ac0f82648284c4d921d2950c7015
Udayrawat007 <uday00203@gmail.com>
2026-02-12 19:04:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+// Scroll Animation
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+hiddenElements.forEach(el => observer.observe(el));
